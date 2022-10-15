ce1fbdfc4ea9c7f7bf98b9b1f6fad497be5e9143
Yasha <yashar@rezilion.com>
2022-10-15 14:45:41 -0400

    Terrain generates an actual cellular terrain

--- source/cellularworld/world/terrain.py
+++ source/cellularworld/world/terrain.py
@@ -1,53 +1,70 @@
+from cellularworld.entities.biome import biome_type
 from cellularworld.world import matrix, noise
+from cellularworld.entities import cell
 
 MAX_HEIGHT = 16
 HEIGHT_RANGE = (0, MAX_HEIGHT)
 EDGE_THICKNESS = 2
 LAND_HARSHNESS = 4
 SMOOTHING_ITERATIONS = 3
-
+WATER_LEVEL = 9
 SIZE = 35
 
 
 class Terrain:
     def generate(self, seed):
-        terrain = matrix.Matrix(SIZE, SIZE)
-        self._create(terrain, seed)
+        height_map = matrix.Matrix(SIZE, SIZE)
+        self._create(height_map, seed)
         for iteration in range(SMOOTHING_ITERATIONS):
-            self._smooth_edges(terrain)
-        self._descretify(terrain)
+            self._smooth_edges(height_map)
+        self._descretify(height_map)
+        terrain = self._generate_cells(height_map)
+        return terrain
+    
+    def _generate_cells(self, height_map):
+        terrain = matrix.Matrix(SIZE, SIZE)
+        for row in range(height_map.width):
+            for column in range(height_map.height):
+                height = height_map[row][column]
+                biome = self._biome(height)
+                terrain[row][column] = cell.Cell(biome, height)
         return terrain
 
-    def _descretify(self, terrain):
-        for row in range(SIZE):
-            for column in range(SIZE):
-                terrain[row][column] = round(terrain[row][column])
+    def _descretify(self, height_map):
+        for row in range(height_map.width):
+            for column in range(height_map.height):
+                height_map[row][column] = round(height_map[row][column])
 
-    def _smooth_edges(self, terrain):
-        for edge in self._edges(SIZE):
-            for point in range(SIZE):
-                row, column = edge, point
-                self._smooth(column, row, terrain)
-                row, column = point, edge
-                self._smooth(column, row, terrain)
+    def _smooth_edges(self, height_map):
+        for row in self._edges(height_map.height):
+            for column in range(height_map.width):
+                self._smooth(row, column, height_map)
 
-    def _smooth(self, column, row, terrain):
-        terrain[row][column] = self._neighbourhood_average(terrain, row, column)
+        for column in self._edges(height_map.width):
+            for row in range(height_map.height):
+                self._smooth(row, column, height_map)
 
-    def _create(self, terrain, seed):
-        width, height = SIZE, SIZE
-        noise_map = noise.Noise.generate(LAND_HARSHNESS, seed, width, height)
+    def _smooth(self, row, column, height_map):
+        height_map[row][column] = self._neighbourhood_average(height_map, row, column)
+
+    def _create(self, height_map, seed):
+        noise_map = noise.Noise.generate(LAND_HARSHNESS, seed, height_map.width, height_map.height)
         noise_range = noise.Noise.range(noise_map)
-        for row in range(height):
-            for column in range(width):
+        for row in range(height_map.width):
+            for column in range(height_map.height):
                 point_value = noise_map[row][column]
-                terrain[row][column] = self._linear_interpolation(*noise_range, *HEIGHT_RANGE, point_value)
+                height_map[row][column] = self._linear_interpolation(*noise_range, *HEIGHT_RANGE, point_value)
 
-    def _neighbourhood_average(self, terrain, row, column):
-        average = terrain[row][column]
-        neighbour_coordinates = terrain.neighbour_coordinates(row, column)
+    def _biome(self, height):
+        if height <= WATER_LEVEL:
+            return biome_type.BiomeType.SEA
+        return biome_type.BiomeType.LAND
+
+    def _neighbourhood_average(self, height_map, row, column):
+        average = height_map[row][column]
+        neighbour_coordinates = height_map.neighbour_coordinates(row, column)
         for neighbour_row, neighbour_column in neighbour_coordinates:
-            average += terrain[neighbour_row][neighbour_column]
+            average += height_map[neighbour_row][neighbour_column]
         average /= len(neighbour_coordinates) + 1
         return average
 
