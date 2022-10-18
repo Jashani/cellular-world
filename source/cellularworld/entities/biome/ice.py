18f629749c4cd12d4e0ef50793a8fcaef4c77c3b
Yasha <yashar@rezilion.com>
2022-10-18 19:32:22 -0400

    Fix ice melting implementation

--- source/cellularworld/entities/biome/ice.py
+++ source/cellularworld/entities/biome/ice.py
@@ -8,8 +8,8 @@ ICE_STAYING_FROZEN_AT = -0
 class Ice(base.Base):
     def __init__(self, cell):
         super().__init__(cell)
-        self._density = 1  # Default?
-        self._future_density = 1
+        self._density = 150  # Default?
+        self._future_density = 150
 
     def __copy__(self):
         cls = self.__class__
@@ -36,11 +36,14 @@ class Ice(base.Base):
 
     def _warm_up(self):
         neighbouring_icebergs = self._neighbouring_icebergs_count()
-        self._future_density -= self.cell.state.temperature * config.effects.elements.heat.on.ice * (1/(neighbouring_icebergs + 1))
+        neighbourly_love = 1 / (neighbouring_icebergs + 1)
+        self._future_density -= self.cell.state.temperature * config.effects.elements.heat.on.ice * neighbourly_love
 
     def _cool_down(self):
         neighbouring_icebergs = self._neighbouring_icebergs_count()
-        self._future_density += self.cell.state.temperature * config.effects.elements.heat.on.ice * (1 - (1/(neighbouring_icebergs + 1)))
+        neighbourly_love = 1 - (1 / (neighbouring_icebergs + 1))
+        added_density = -1 * (self.cell.state.temperature * config.effects.elements.heat.on.ice * neighbourly_love) / self._density
+        self._future_density += added_density
 
     def _neighbouring_icebergs_count(self):
         count = len([neighbour for neighbour in self.cell.neighbours if type(neighbour.biome) == type(self)])
