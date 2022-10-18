bfdc158df549e30ba0cbd15be38dc63587715b46
Yasha <yashar@rezilion.com>
2022-10-18 18:18:05 -0400

    Oceans absorb heat

--- source/cellularworld/entities/biome/sea.py
+++ source/cellularworld/entities/biome/sea.py
@@ -8,6 +8,11 @@ class Sea(base.Base):
         return biome_type.BiomeType.SEA
 
     def do_cycle(self):
+        self.absorb()
         self.cell.future_state.cloud_density += max(0, self.cell.state.temperature * config.effects.elements.heat.on.cloud_creation)
         if self.cell.state.temperature <= config.thresholds.biomes.sea.freezing:
             self.cell.set_biome(biome_type.BiomeType.ICE)
+
+    def absorb(self):
+        self.cell.future_state.temperature -= config.effects.biomes.sea.on.heat
+        self.cell.future_state.pollution -= config.effects.biomes.sea.on.pollution
