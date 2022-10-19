c91270939a093737f3002cc6723d276b3572cdeb
Yasha <yashar@rezilion.com>
2022-10-18 21:32:30 -0400

    I MANAGED TO STABILISE WIND THIS IS THE GREATEST MOMENT OF MY LIFE HOLY SHIT I CREATED AN ACTUAL FUCKING WIND SYSTEM BRO THIS IS ABSOLUTELY INCREDIBLE HAHAHAHAHAHAHAHAHAHAHA

--- source/cellularworld/entities/wind.py
+++ source/cellularworld/entities/wind.py
@@ -7,12 +7,13 @@ import random
 
 RELATIVE_DIRECTION_ANGLE_FACTOR = 45
 ZERO_DIRECTION = numpy.array([1., 0.])
+CIRCLE = 360
 
 
 class Wind:
     def __init__(self, cell):
         self._cell = cell
-        wind = [3., 4.]#[random.uniform(3, 4), random.uniform(3, 4)]
+        wind = [random.uniform(0, 0.5), random.uniform(0, 0.5)] #[0., 0.]
         self._strength = numpy.array(wind)
         self.future_strength = numpy.array(wind)
 
@@ -34,7 +35,7 @@ class Wind:
         return vector_tools.magnitude(self._strength)
 
     def do_cycle(self):
-        self._blow_things_away()
+        # self._blow_things_away()
         self._go_with_the_flow()
 
     def commit(self):
@@ -51,12 +52,12 @@ class Wind:
         for index, neighbour in enumerate(self._cell.neighbours):
             neighbour_effect, dissipation_factor = self._neighbour_effect(index, neighbour.wind)
             neighbour_effect_magnitude = numpy.linalg.norm(neighbour_effect)
-            unit_vector = self.unit_vector(neighbour_effect)
-            self.wind_heat_interactions(neighbour, unit_vector)
+            towards_me = self._towards_me(index)
+            self.wind_heat_interactions(neighbour, towards_me)
             if neighbour_effect_magnitude == 0 and dissipation_factor == 0:
                 continue
-            self.affect_wind(dissipation_factor, neighbour, neighbour_effect, unit_vector)
-            self.affect_elements(dissipation_factor, neighbour, neighbour_effect_magnitude)
+            self.affect_wind(dissipation_factor, neighbour, neighbour_effect, towards_me)
+            # self.affect_elements(dissipation_factor, neighbour, neighbour_effect_magnitude)
 
     def wind_heat_interactions(self, neighbour, towards_me):
         temperature_difference = neighbour.state.temperature - self._cell.state.temperature
@@ -81,28 +82,38 @@ class Wind:
         self._cell.future_state.cloud_density += min(cloud_effect, neighbour.state.cloud_density) * dissipation_factor
 
     def _neighbour_effect(self, neighbour_index, wind):
+        relevant_directions = self._relevant_directions(wind)
         my_relative_direction = neighbour_index * RELATIVE_DIRECTION_ANGLE_FACTOR
+        relevant_directions_absolutes = [direction % CIRCLE for direction in relevant_directions]
+        if my_relative_direction not in relevant_directions_absolutes:
+            return 0, 0
         my_dissipation_factor = self._dissipation_factor(my_relative_direction, wind.direction_angle)
         if my_dissipation_factor == 0:
             return 0, 0
-        normalised_dissipation_factor = self._my_normalised_dissipation_factor(my_dissipation_factor, my_relative_direction, wind)
+        normalised_dissipation_factor = self._my_normalised_dissipation_factor(my_dissipation_factor, relevant_directions, wind.direction_angle)
         neighbour_effect = wind.strength * normalised_dissipation_factor * config.effects.elements.wind.travel_dissipation
         return neighbour_effect, normalised_dissipation_factor
 
-    def _my_normalised_dissipation_factor(self, my_dissipation_factor, my_relative_direction, wind):
-        left_neighbour_relative_direction = (my_relative_direction + RELATIVE_DIRECTION_ANGLE_FACTOR)
-        right_neighbour_relative_direction = (my_relative_direction - RELATIVE_DIRECTION_ANGLE_FACTOR)
-        left_neighbour_dissipation_factor = self._dissipation_factor(left_neighbour_relative_direction, wind.direction_angle)
-        right_neighbour_dissipation_factor = self._dissipation_factor(right_neighbour_relative_direction, wind.direction_angle)
-        factor_sum = my_dissipation_factor + left_neighbour_dissipation_factor + right_neighbour_dissipation_factor
+    def _my_normalised_dissipation_factor(self, my_dissipation_factor, relevant_directions, wind_direction):
+        factor_sum = self._dissipation_factor_sum(relevant_directions, wind_direction)
         normalised_dissipation_factor = my_dissipation_factor / factor_sum
         return normalised_dissipation_factor
+
+    def _dissipation_factor_sum(self, directions, wind_direction):
+        factors = [self._dissipation_factor(direction, wind_direction) for direction in directions]
+        return sum(factors)
 
     def _dissipation_factor(self, location_direction, wind_direction):
         if abs(location_direction - wind_direction) > (config.effects.elements.wind.dissipation_cone / 2):
             return 0
         dissipation_factor = math.pow(math.cos(2 * (location_direction - wind_direction)), config.effects.elements.wind.dissipation_limiter)
         return dissipation_factor
+
+    def _relevant_directions(self, wind):
+        cardinal_direction = round(wind.direction_angle / RELATIVE_DIRECTION_ANGLE_FACTOR)
+        relevant_cardinal_directions = [cardinal_direction, cardinal_direction + 1, cardinal_direction - 1]
+        relevant_directions = [RELATIVE_DIRECTION_ANGLE_FACTOR * direction for direction in relevant_cardinal_directions]
+        return relevant_directions
 
     @property
     def direction_angle(self):
