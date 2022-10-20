789a931041f1a6fe94910c5db919b9e8d7489853
Yasha <yashar@rezilion.com>
2022-10-19 22:23:46 -0400

    Optimise wind effect on elements

--- source/cellularworld/entities/wind.py
+++ source/cellularworld/entities/wind.py
@@ -13,7 +13,7 @@ CIRCLE = 360
 class Wind:
     def __init__(self, cell):
         self._cell = cell
-        wind = [random.uniform(0, 0.5), random.uniform(0, 0.5)] #[0., 0.]
+        wind = [0., 0.]
         self._strength = numpy.array(wind)
         self.future_strength = numpy.array(wind)
 
@@ -35,29 +35,28 @@ class Wind:
         return vector_tools.magnitude(self._strength)
 
     def do_cycle(self):
-        # self._blow_things_away()
+        self._blow_things_away()
         self._go_with_the_flow()
 
     def commit(self):
         self._strength = copy.deepcopy(self.future_strength)
 
     def _blow_things_away(self):
-        strength = self.strength_magnitude()
-        self._cell.future_state.pollution -= strength * config.effects.elements.wind.on.pollution
-        self._cell.future_state.temperature -= strength * config.effects.elements.wind.on.heat
-        self._cell.future_state.cloud_density -= strength * config.effects.elements.wind.on.clouds
+        strength_magnitude = self.strength_magnitude()
+        self._cell.future_state.pollution -= strength_magnitude * config.effects.elements.wind.on.pollution
+        self._cell.future_state.cloud_density -= strength_magnitude * config.effects.elements.wind.on.clouds
 
     def _go_with_the_flow(self):
         self.future_strength = numpy.array([0., 0.])
         for index, neighbour in enumerate(self._cell.neighbours):
             neighbour_effect, dissipation_factor = self._neighbour_effect(index, neighbour.wind)
-            neighbour_effect_magnitude = numpy.linalg.norm(neighbour_effect)
+            neighbour_effect_magnitude = vector_tools.magnitude(neighbour_effect)
             towards_me = self._towards_me(index)
             self.wind_heat_interactions(neighbour, towards_me)
             if neighbour_effect_magnitude == 0 and dissipation_factor == 0:
                 continue
             self.affect_wind(dissipation_factor, neighbour, neighbour_effect, towards_me)
-            # self.affect_elements(dissipation_factor, neighbour, neighbour_effect_magnitude)
+            self.affect_elements(dissipation_factor, neighbour, neighbour_effect_magnitude)
 
     def wind_heat_interactions(self, neighbour, towards_me):
         temperature_difference = neighbour.state.temperature - self._cell.state.temperature
@@ -73,13 +72,10 @@ class Wind:
             self.future_strength += towards_me * ((neighbour.height - self._cell.height) * config.effects.elements.height.on.wind) * dissipation_factor
 
     def affect_elements(self, dissipation_factor, neighbour, neighbour_effect_magnitude):
-        temperature_difference = neighbour.state.temperature - self._cell.state.temperature
-        temperature_effect = temperature_difference * neighbour_effect_magnitude * config.effects.elements.wind.on.heat
-        pollution_effect = neighbour_effect_magnitude * neighbour.state.pollution * config.effects.elements.wind.on.pollution
-        cloud_effect = neighbour_effect_magnitude * neighbour.state.cloud_density * config.effects.elements.wind.on.clouds
-        self._cell.future_state.temperature += min(temperature_difference, temperature_effect) * dissipation_factor
-        self._cell.future_state.pollution += min(pollution_effect, neighbour.state.pollution) * dissipation_factor
-        self._cell.future_state.cloud_density += min(cloud_effect, neighbour.state.cloud_density) * dissipation_factor
+        pollution_effect = neighbour_effect_magnitude * config.effects.elements.wind.on.pollution
+        cloud_effect = neighbour_effect_magnitude * config.effects.elements.wind.on.clouds
+        self._cell.future_state.pollution += min(pollution_effect, abs(neighbour.state.pollution * dissipation_factor))
+        self._cell.future_state.cloud_density += min(cloud_effect, abs(neighbour.state.cloud_density * dissipation_factor))
 
     def _neighbour_effect(self, neighbour_index, wind):
         relevant_directions = self._relevant_directions(wind)
