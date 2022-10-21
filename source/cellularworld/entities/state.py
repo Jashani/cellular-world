5f10a38396f0f4f4c6d01c9d9fa1a4794e4ae7d6
Yasha <yashar@rezilion.com>
2022-10-21 10:10:04 -0400

    Use references in state to not look at post-calculation values

--- source/cellularworld/entities/state.py
+++ source/cellularworld/entities/state.py
@@ -13,21 +13,21 @@ class State:
         self.pollution = config.world.start.pollution
         self.cloud_density = config.world.start.clouds
 
-    def is_raining(self):
-        rain_threshold = config.thresholds.elements.rain.base + max(0, self.temperature * config.effects.elements.heat.on.rain)
-        return self.cloud_density >= rain_threshold
+    def is_raining(self, reference_state):
+        rain_threshold = config.thresholds.elements.rain.base + max(0, reference_state.temperature * config.effects.elements.heat.on.rain)
+        return reference_state.cloud_density >= rain_threshold
 
-    def do_cycle(self):
-        cloud_blockage = max(1, self.cloud_density * config.effects.elements.clouds.on.sun)
-        temperature, pollution = self.temperature, self.pollution
+    def do_cycle(self, reference_state):
+        cloud_blockage = max(1, reference_state.cloud_density * config.effects.elements.clouds.on.sun)
+        temperature, pollution = reference_state.temperature, reference_state.pollution
         self.temperature += config.effects.elements.sun.on.heat / cloud_blockage
         self.temperature += pollution * config.effects.elements.pollution.on.heat
         self.pollution += temperature * config.effects.elements.heat.on.pollution
-        if not self.is_raining():
+        if not self.is_raining(reference_state):
             return
         self.temperature -= config.effects.elements.rain.on.heat
         self.pollution -= config.effects.elements.rain.on.pollution
-        self.cloud_density -= config.effects.elements.rain.on.cloud_reduction * (self.cloud_density / config.effects.elements.rain.on.cloud_reduction)
+        self.cloud_density -= config.effects.elements.rain.on.cloud_reduction * ((reference_state.cloud_density**2) / 25)
 
     def validate_values(self):
         self.cloud_density = max(0, self.cloud_density)
